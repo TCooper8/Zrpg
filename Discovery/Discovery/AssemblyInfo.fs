0cd854b8483f5cd510a65617e06f73a6dd3573b0
TCooper8 <coopetyl@live.com>
2016-04-11 23:07:09 -0600

    Fixed assembly reference path for discovery.

--- Discovery/Discovery/AssemblyInfo.fs
+++ Discovery/Discovery/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace CloudClient.AssemblyInfo
+﻿namespace Discovery.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("CloudClient")>]
+[<assembly: AssemblyTitle("Discovery")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("CloudClient")>]
+[<assembly: AssemblyProduct("Discovery")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
