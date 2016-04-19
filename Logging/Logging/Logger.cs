d016d6ac9f97f101e3176e581f92ece9fa7ca9f5
TCooper8 <coopetyl@live.com>
2016-04-18 20:47:41 -0600

    Updated Logger

--- Logging/Logging/Logger.cs
+++ Logging/Logging/Logger.cs
@@ -13,13 +13,16 @@ namespace Logging
   static class LogManager
   {
     private static Dictionary<string, Task> tasks =
-        new Dictionary<string, Task>();
+      new Dictionary<string, Task>();
 
     private static Dictionary<string, ConcurrentQueue<string>> queues =
-        new Dictionary<string, ConcurrentQueue<string>>();
+      new Dictionary<string, ConcurrentQueue<string>>();
+
+    private static Dictionary<string, int> queueRefCounts =
+      new Dictionary<string, int>();
 
     private static Dictionary<string, CancellationTokenSource> tokens =
-        new Dictionary<string, CancellationTokenSource>();
+      new Dictionary<string, CancellationTokenSource>();
 
     private static Object objLock =
       new object();
@@ -27,20 +30,22 @@ namespace Logging
     static async Task BeginPolling(
         ConcurrentQueue<string> queue,
         CancellationToken token,
-        Stream dstStream,
-        StreamWriter outStream
+        Stream dstStream
         )
     {
       string msg;
       StringBuilder builder;
       int n = 0;
 
-      while (!token.IsCancellationRequested)
+      while (true)
       {
+        if (token.IsCancellationRequested && queue.Count == 0)
+        {
+          break;
+        }
+
         if (queue.TryDequeue(out msg))
         {
-          //await outStream.WriteLineAsync(msg);
-
           builder = new StringBuilder(msg + "\n");
           n = msg.Length;
           int msgs = 1;
@@ -61,7 +66,7 @@ namespace Logging
 
           var buf = Encoding.UTF8.GetBytes(finalMsg);
           //Console.WriteLine(finalMsg);
-#if DEBUG
+#if DEBUG || TRACE
           Debug.Write(finalMsg);
 #endif
           await dstStream.WriteAsync(buf, 0, buf.Length);
@@ -71,6 +76,8 @@ namespace Logging
           await Task.Delay(16);
         }
       }
+
+      // Token was cancelled.
     }
 
     public static ConcurrentQueue<string> CreateStreamLogger(string key, Stream stream)
@@ -79,6 +86,18 @@ namespace Logging
 
       lock (objLock)
       {
+        // Increment the reference counter.
+        int refCount;
+        if (queueRefCounts.TryGetValue(key, out refCount))
+        {
+          refCount++;
+          queueRefCounts[key] = refCount;
+        }
+        else
+        {
+          queueRefCounts.Add(key, 1);
+        }
+
         if (queues.TryGetValue(key, out queue))
         {
           return queue;
@@ -94,20 +113,56 @@ namespace Logging
         {
           try
           {
-            await BeginPolling(queue, tok, stream, outStream);
+            await BeginPolling(queue, tok, stream);
           }
           catch (Exception _)
           {
             outStream.Close();
           }
         });
-
         queues.Add(key, queue);
         tokens.Add(key, tokSource);
         tasks.Add(key, task);
       }
 
       return queue;
+    }
+
+    public static void CloseLogger(string key)
+    {
+      lock (objLock)
+      {
+        int refCount;
+        if (!queueRefCounts.TryGetValue(key, out refCount))
+        {
+          // This has already been cancelled.
+          return;
+        }
+
+        // Decrement the counter.
+        refCount--;
+        queueRefCounts[key] = refCount;
+
+        if (refCount > 0)
+        {
+          // We can't cancel the queue yet.
+          return;
+        }
+
+        CancellationTokenSource source;
+        if (!tokens.TryGetValue(key, out source))
+        {
+          // No token, this has been cancelled already.
+          return;
+        }
+
+        source.Cancel(false);
+
+        queueRefCounts.Remove(key);
+        queues.Remove(key);
+        tokens.Remove(key);
+        tasks.Remove(key);
+      }
     }
   }
 
@@ -119,7 +174,7 @@ namespace Logging
     Debug = 50,
   }
 
-  public abstract class Logger
+  public abstract class Logger : IDisposable
   {
     abstract public LogLevel Level { get; }
     abstract public string Name { get; }
@@ -179,6 +234,11 @@ namespace Logging
     public void Debug(string format, params object[] args)
     {
       Log(LogLevel.Debug, format, args);
+    }
+
+    public void Dispose()
+    {
+      LogManager.CloseLogger(this.Name);
     }
   }
 
