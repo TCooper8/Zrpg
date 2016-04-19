66cdeef1217c694915ab8732df60eee0e17200f2
TCooper8 <coopetyl@live.com>
2016-04-19 16:49:17 -0600

    Updated GameServer

--- GameServer/GameServer/GameServer.fs
+++ GameServer/GameServer/GameServer.fs
@@ -31,13 +31,12 @@ module GameServer =
 
   type Cmd = Msg * AsyncReplyChannel<Reply>
 
-  type GameServer () =
-    let log = StreamLogger(
-      "GameServer",
-      LogLevel.Debug,
-      Console.OpenStandardOutput()
-    )
+  [<Interface>]
+  type IGameServer =
+    abstract ApiHandler: Pario.WebServer.Handler
+    abstract WebSocketHandler: Net.HttpListenerContext -> bool Async
 
+  type private GameServer (log:Logger) =
     let gameState = GameState()
     let game = GameRunner.Game(gameState, log)
     let webServer = Pario.WebServer.Server(log)
@@ -50,32 +49,35 @@ module GameServer =
         let! cmd = inbox.Receive()
         let msg, reply = cmd
 
-        log.Debug <| sprintf "Received \n\t%A" cmd
+        let res: Reply =
+          try
+            log.Debug <| sprintf "Received \n\t%A" msg
+            match msg with
+            | AddGarrison msg ->
+              let stats = {
+                goldIncome = 10
+                heroes = []
+              }
 
-        match msg with
-        | AddGarrison msg ->
-          let stats = {
-            goldIncome = 10
-            heroes = []
-          }
+              let garrison = {
+                id = uuid()
+                clientId = "tmpClientId"
+                name = msg.name
+                race = msg.race
+                faction = msg.faction
+                ownedRegions = Set []
+                ownedZones = Set []
+                vaultId = ""
+                stats = stats
+              }
 
-          let garrison = {
-            id = uuid()
-            clientId = "tmpClientId"
-            name = msg.name
-            race = msg.race
-            faction = msg.faction
-            ownedRegions = Set []
-            ownedZones = Set []
-            vaultId = ""
-            stats = stats
-          }
+              match gameState.addGarrison garrison with
+              | Failure e -> ExnReply e
+              | Success () -> MsgReply "Garrison created!"
+          with e ->
+            ExnReply e
 
-          let res = match gameState.addGarrison garrison with
-          | Failure e -> ExnReply e
-          | Success () -> MsgReply "Garrison created!"
-
-          reply.Reply(res)
+        reply.Reply(res)
 
         return! loop ()
       }
@@ -83,41 +85,6 @@ module GameServer =
     )
 
     let enc = Encoding.UTF8
-
-    do webServer.handle {
-      priority = 100
-      handler = fun req resp -> async {
-        let path = req.Url.AbsolutePath
-
-        if path <> "/api" then
-          return false
-        else
-        try
-          // This is an addCharacterRequest.
-          use input = req.InputStream
-          use reader = new StreamReader(input)
-
-          let! json = reader.ReadToEndAsync() |> Async.AwaitTask
-
-          log.Debug <| sprintf "Got json %s" json
-
-          let msg = JsonConvert.DeserializeObject<Msg>(json)
-
-          // Now, issue the command to the server.
-          let! res = agent.PostAndAsyncReply(fun reply -> (msg, reply))
-
-          use resp = resp.OutputStream
-          let json = JsonConvert.SerializeObject(res) |> enc.GetBytes
-          do! resp.WriteAsync(json, 0, json.Length) |> Async.AwaitIAsyncResult |> Async.Ignore
-
-          return true
-
-        with e ->
-          resp.StatusCode <- 400
-          resp.StatusDescription <- e.Message
-          return true
-      }
-    }
 
     let handleWs (ws:WebSocket) = async {
       use ws = ws
@@ -162,31 +129,56 @@ module GameServer =
       return ()
     }
 
-    do webServer.handleSocket <| fun context -> async {
-      try
-        let! wsCtx = context.AcceptWebSocketAsync(null) |> Async.AwaitTask
+    interface IGameServer with
+      member this.ApiHandler: Pario.WebServer.Handler = fun req resp -> async {
+        let path = req.Url.AbsolutePath
 
-        let ws = wsCtx.WebSocket
-        handleWs ws |> Async.Start
+        if path <> "/api" then
+          return false
+        else
+        try
+          // This is an addCharacterRequest.
+          use input = req.InputStream
+          use reader = new StreamReader(input)
 
-        return true
-      with e ->
-        log.Warn("Encountered a WebSocket error: {0}", e)
-        return false
-    }
+          let! json = reader.ReadToEndAsync() |> Async.AwaitTask
 
-    let createServer (log:Logger) = async {
-    }
+          log.Debug <| sprintf "Got json %s" json
 
-    member this.Listen host port = async {
-      let! res = discovery.addServiceHost "gameserver" {
-        ipAddress = host
-        port = port
-        hostType = "http"
-        status = "good"
+          let msg = JsonConvert.DeserializeObject<Msg>(json)
+
+          if obj.ReferenceEquals(msg, null) then
+            failwith "Unable to deserialize message"
+            return true
+          else
+          // Now, issue the command to the server.
+          let! res = agent.PostAndAsyncReply(fun reply -> (msg, reply))
+
+          use resp = resp.OutputStream
+          let json = JsonConvert.SerializeObject(res) |> enc.GetBytes
+          do! resp.WriteAsync(json, 0, json.Length) |> Async.AwaitIAsyncResult |> Async.Ignore
+
+          return true
+
+        with e ->
+          resp.StatusCode <- 400
+          resp.StatusDescription <- e.Message
+          return true
       }
 
-      Option.iter (fun e -> raise e) res
+      member this.WebSocketHandler (context: Net.HttpListenerContext) = async {
+      //do webServer.handleSocket <| fun context -> async {
+        try
+          let! wsCtx = context.AcceptWebSocketAsync(null) |> Async.AwaitTask
 
-      do! webServer.listen host port
-    }
+          let ws = wsCtx.WebSocket
+          handleWs ws |> Async.Start
+
+          return true
+        with e ->
+          log.Warn("Encountered a WebSocket error: {0}", e)
+          return false
+      }
+
+  let server log =
+    GameServer(log) :> IGameServer
