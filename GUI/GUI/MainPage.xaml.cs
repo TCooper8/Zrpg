33421b7d4dd3e239155342a02a80eec67cb3d260
breaud <dylan.breaud@gmail.com>
2016-04-29 18:24:00 -0700

    Fixed bug on login if server wasn't available

--- GUI/GUI/MainPage.xaml.cs
+++ GUI/GUI/MainPage.xaml.cs
@@ -100,26 +100,32 @@ namespace GUI
             relativePanel.Visibility = Visibility.Visible; 
             progressRing.IsActive = true;
             await Task.Delay(2000);
-            
-            // This should populate the clientId.
-            state.Authenticate(
-                this.usernameTextBox.Text,
-                this.passwordBox.Password
-            );
 
-            // After authenticating, check to see if the client has a garrison already.
-            var reply = await state.GetGarrison();
-            if (reply.IsEmpty)
+            // This should populate the clientId.
+            try
             {
-                // The client has no Garrison, take them to create a new one.
-                this.Frame.Navigate(typeof(ChooseFactionPage));
-                return;
-            }
-            else
+                state.Authenticate(
+                    this.usernameTextBox.Text,
+                    this.passwordBox.Password
+                    );
+
+                // After authenticating, check to see if the client has a garrison already.
+                var reply = await state.GetGarrison();
+                if (reply.IsEmpty)
+                {
+                    // The client has no Garrison, take them to create a new one.
+                    this.Frame.Navigate(typeof(ChooseFactionPage));
+                    return;
+                }
+                else
+                {
+                    // The client has a garrison, take them to the garrison page.
+                    this.Frame.Navigate(typeof(GarrisonPage));
+                }
+            }    
+            catch
             {
-                // The client has a garrison, take them to the garrison page.
-                this.Frame.Navigate(typeof(GarrisonPage));
-            }
+            }       
         }
 
         private void registerButton_Click(object sender, RoutedEventArgs e)
